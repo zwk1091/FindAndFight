8884782fcbab3c4073dae6ae8e24b87d28ac6d47
Wenkun <zhuwk3@mail2.sysu.edu.cn>
2017-12-07 23:52:23 +0800

    12/7/23:50

---
+++ app/src/main/java/com/example/findandfight/IndoorModeActivity.java
@@ -0,0 +1,17 @@
+package com.example.findandfight;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 焜 on 2017/12/7.
+ */
+
+public class IndoorModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/example/findandfight/MainActivity.java
+++ app/src/main/java/com/example/findandfight/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.findandfight;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button indoorMode=(Button)findViewById(R.id.nationalMode);
+        indoorMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,NationalModeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
